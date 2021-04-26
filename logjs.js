3f039773b501cd31c43b6c019faf682b912ae88e
MaksPunch <75011198+MaksPunch@users.noreply.github.com>
2021-04-26 22:31:51 +0300

    Update logjs.js

--- logjs.js
+++ logjs.js
@@ -31,32 +31,9 @@ let equals = document.querySelector(".equals");
 	const e = 2.71828;
 
 	let base1 = Number(document.querySelector("#base1").value);
-	let base2 = Number(document.querySelector('#base2'));
-	let base3 = Number(document.querySelector('#base3'));
-	let base4 = Number(document.querySelector('#base4'));
-	let base5 = Number(document.querySelector('#base5'));
-	let base6 = Number(document.querySelector('#base6'));
-	let base7 = Number(document.querySelector('#base7'));
-	let base8 = Number(document.querySelector('#base8'));
-	let base9 = Number(document.querySelector('#base9'));
-	let base10 = Number(document.querySelector('#base10'));
-	let base11 = Number(document.querySelector('#base11'));
-	let base12 = Number(document.querySelector('#base12'));
-	let base13 = Number(document.querySelector('#base13'));
 
 	let number1 = Number(document.querySelector('#number1').value);
-	let number2 = Number(document.querySelector('#number2'));
-	let number3 = Number(document.querySelector('#number3'));
-	let number4 = Number(document.querySelector('#number4'));
-	let number5 = Number(document.querySelector('#number5'));
-	let number6 = Number(document.querySelector('#number6'));
-	let number7 = Number(document.querySelector('#number7'));
-	let number8 = Number(document.querySelector('#number8'));
-	let number9 = Number(document.querySelector('#number9'));
-	let number10 = Number(document.querySelector('#number10'));
-	let number11 = Number(document.querySelector('#number11'));
-	let number12 = Number(document.querySelector('#number12'));
-	let number13 = Number(document.querySelector('#number13'));
+
 
 	let loganswer = document.querySelector('.loganswer');
 	loganswer.classList.remove('hidden');
